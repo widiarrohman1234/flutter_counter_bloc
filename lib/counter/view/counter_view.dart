2bfdcc8ff0791e62529e4377282cfe75262f8000
Widi Arrohman <arrohmanwidi@gmail.com>
2026-05-04 10:09:02 +0700

    update: basic banget

--- lib/counter/view/counter_view.dart
+++ lib/counter/view/counter_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_counter_bloc/counter/cubit/counter_cubit.dart';
-import 'package:flutter_counter_bloc/counter/view/counter_page.dart';
 
 class CounterView extends StatelessWidget {
   const CounterView({super.key});
