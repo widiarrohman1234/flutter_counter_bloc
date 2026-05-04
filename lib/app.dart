d937e5289855b70ab1d8fd065f8ccc0341c9abcb
Widi Arrohman <arrohmanwidi@gmail.com>
2026-05-04 10:18:39 +0700

    update: counter and home page bloc cubit

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_counter_bloc/counter/counter.dart';
-// import 'package:flutter_counter_bloc/counter/view/counter_page.dart';
+import 'package:flutter_counter_bloc/counter/cubit/counter_cubit.dart';
 import 'package:flutter_counter_bloc/home/view/home_page.dart';
-import 'package:flutter_counter_bloc/home/view/home_view.dart';
+import 'package:flutter_counter_bloc/counter/view/counter_page.dart';
 
 class CounterApp extends StatelessWidget {
   const CounterApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [BlocProvider(create: (_) => CounterCubit())],
-      child: HomeView(),
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (_) => const HomePage(),
+          '/counter': (_) => const CounterPage(),
+        },
+      ),
     );
   }
 }
