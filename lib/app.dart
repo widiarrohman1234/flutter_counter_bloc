1239a82fe6706ccef13f25e5bfdb26fd2ae92e9f
Widi Arrohman <arrohmanwidi@gmail.com>
2026-05-05 13:33:41 +0700

    update praktikum B tahap 1

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_counter_bloc/counter/counter.dart';
+import 'package:flutter_counter_bloc/counter/view/counter_page.dart';
 
 class CounterApp extends MaterialApp {
   const CounterApp({super.key}): super(home: const CounterPage());
