c182346054352ce4e4edf20893ae55027102cc4c
Widi Arrohman <arrohmanwidi@gmail.com>
2026-05-07 12:23:49 +0700

    update praktikum sesi 1.5

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_counter_bloc/counter/cubit/counter_cubit.dart';
 import 'package:flutter_counter_bloc/counter/view/counter_page.dart';
 
-class CounterApp extends MaterialApp {
-  const CounterApp({super.key}): super(home: const CounterPage());
+class CounterApp extends StatelessWidget {
+  const CounterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/counter',
+        routes: {
+          // '/': (_) => const HomePage(),
+          '/counter': (_) => const CounterPage(),
+        },
+      ),
+    );
+  }
 }
---
+++ lib/temp.dart
@@ -0,0 +1,10 @@
+import 'package:bloc/bloc.dart';
+
+class CounterObserver extends BlocObserver {
+  const CounterObserver();
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change){
+    super.onChange(bloc, change);
+  }
+}
