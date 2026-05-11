50472f2c06ca0f3850a3378ca4f20be4b00faafc
Widi Arrohman <arrohmanwidi@gmail.com>
2026-05-11 15:08:19 +0700

    update

--- lib/temp.dart
+++ lib/temp.dart
@@ -1,16 +1,43 @@
-import 'package:bloc/bloc.dart';
-import 'package:flutter_counter_bloc/counter/cubit/counter_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_counter_bloc/core/theme/theme_cubit.dart';
+import 'package:flutter_counter_bloc/counter/counter.dart';
+import 'package:flutter_counter_bloc/home/view/home_page.dart';
+import 'package:flutter_counter_bloc/posts/bloc/post_bloc.dart';
+import 'package:flutter_counter_bloc/posts/bloc/post_event.dart';
+import 'package:flutter_counter_bloc/posts/view/posts_page.dart';
+import 'package:http/http.dart' as http;
 
-class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterState(counter: 0, isEven: true));
+class CounterApp extends StatelessWidget {
+  const CounterApp({super.key});
 
-  void increment() {
-    final newCounter = state.counter + 1;
-    emit(CounterState(counter: newCounter, isEven: newCounter % 2 == 0));
-  }
-
-  void decrement() {
-    final newCounter = state.counter - 1;
-    emit(CounterState(counter: newCounter, isEven: newCounter % 2 == 0));
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => CounterCubit()),
+        BlocProvider(
+          create: (_) =>
+              PostBloc(httpClient: http.Client())..add(PostFetched()),
+        ),
+        BlocProvider(create: (_) => ThemeCubit()),
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, state) {
+          return MaterialApp(
+            theme: ThemeData.light(),
+            darkTheme: ThemeData.dark(),
+            themeMode: context.watch<ThemeCubit>().state,
+            debugShowCheckedModeBanner: false,
+            initialRoute: '/',
+            routes: {
+              '/': (_) => const HomePage(),
+              '/counter': (_) => const CounterPage(),
+              '/posts': (_) => const PostsPage(),
+            },
+          );
+        },
+      ),
+    );
   }
 }
