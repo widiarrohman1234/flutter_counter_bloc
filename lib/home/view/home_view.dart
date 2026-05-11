28a9103f03849426b6e53442db6b81e9135e6ec3
Widi Arrohman <arrohmanwidi@gmail.com>
2026-05-11 14:49:44 +0700

    update

--- lib/home/view/home_view.dart
+++ lib/home/view/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_counter_bloc/core/widgets/app_drawer.dart';
 import 'package:flutter_counter_bloc/counter/cubit/counter_cubit.dart';
+import 'package:flutter_counter_bloc/counter/cubit/counter_state.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -12,13 +13,25 @@ class HomeView extends StatelessWidget {
       appBar: AppBar(title: const Text("Home")),
       drawer: AppDrawer(),
       body: Center(
-        child: BlocBuilder<CounterCubit, int>(
-          builder: (context, state) {
-            return Text(
-              'Counter Value: $state',
-              style: Theme.of(context).textTheme.headlineMedium,
-            );
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            BlocBuilder<CounterCubit, CounterState>(
+              builder: (context, state) {
+                return Text(
+                  'Counter Value: ${state.counter}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            ),
+            BlocSelector<CounterCubit, CounterState, bool>(
+              selector: (state) => state.isEven,
+              builder: (context, isEven) {
+                return Text(isEven ? "Genap" : "Ganjil");
+              },
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/temp.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter_counter_bloc/counter/cubit/counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState(counter: 0, isEven: true));
+
+  void increment() {
+    final newCounter = state.counter + 1;
+    emit(CounterState(counter: newCounter, isEven: newCounter % 2 == 0));
+  }
+
+  void decrement() {
+    final newCounter = state.counter - 1;
+    emit(CounterState(counter: newCounter, isEven: newCounter % 2 == 0));
+  }
+}
